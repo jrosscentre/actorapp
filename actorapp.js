0a3255801a7b181973c927eef79cadca012bb3a9
J Ross <j.ross@centre.edu>
2019-10-10 11:01:25 -0400

    initial effort

--- actorapp.js
+++ actorapp.js
@@ -1,1 +1,25 @@
+movienamespromise = d3.json("http://ghibliapi.herokuapp.com/films");
+
+
+
+movienamespromise.then(
+
+    
+    
+    function(mvnms){
+        
+        entermvnms(mvnms)
+        
+        console.log("good", (mvnms))
+    }
+)
+var entermvnms = function (mvnms)
+{
+d3.select("#movienames")
+.selectAll("span")
+.data(mvnms)
+.enter()
+.append("span")
+}
+
 
