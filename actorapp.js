d6ab090e4ad99d109e5be2cd4993b838014aa6fb
J Ross <j.ross@centre.edu>
2019-10-14 23:50:05 -0400

    sorta

--- actorapp.js
+++ actorapp.js
@@ -1,25 +1,54 @@
 movienamespromise = d3.json("http://ghibliapi.herokuapp.com/films");
-
+moviedscrptnpromise = d3.json("http://ghibliapi.herokuapp.com/films")
 
 
 movienamespromise.then(
 
-    
+ 
     
     function(mvnms){
         
         entermvnms(mvnms)
-        
+      
+       
         console.log("good", (mvnms))
     }
 )
+
+
 var entermvnms = function (mvnms)
 {
-d3.select("#movienames")
-.selectAll("span")
+d3.select("#mvtitles")
+.selectAll("ul")
 .data(mvnms)
 .enter()
-.append("span")
+.append("li")
+.text(function (d){return d.title;})
+    .on("click", function (d){enterdscrptns(d)})
+
+
+}
+
+
+moviedscrptnpromise.then(
+    
+    function(mvdscrptns){
+        
+        enterdscrptns(mvdscrptns)
+        console.log("good", (mvdscrptns))
+
+    }
+)
+
+var enterdscrptns =function (dscrptns)
+{
+    d3.select("#mvactors")
+    .selectAll("ol")
+    .data(dscrptns)
+    .enter()
+    .append("li")
+    .text(function (d){return d.description})
+    
 }
 
 
